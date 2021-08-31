b97efaa4cf318d2bc22777beace80dc6c11c3758
insidus341 <61412381+insidus341@users.noreply.github.com>
2021-08-31 15:01:34 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,11 +10,21 @@ metrics = PrometheusMetrics(app)
 
 metrics.info('app_info', 'Application info', version='1.0.3')
 
+# custom metric
+by_path_counter = metrics.counter(
+    'by_path_counter', 'Request count by request paths',
+    labels={'path': lambda: request.path}
+)
+
+# custom gauge metric
+info = metrics.info('dynamic_info', 'Something dynamic')
+info.set(42.1)
 
 endpoints = ('one', 'two', 'three', 'four', 'five', 'error')
 
 
 @app.route('/one')
+@by_path_counter
 def first_route():
     time.sleep(random.random() * 0.2)
     return 'ok'
